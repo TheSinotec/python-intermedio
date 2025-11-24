7a4aecbd05a8e663810c9eee6b6ea12c52fd4313
Jonatan Salgado Razo <likelas@ciencias.unam.mx>
2025-11-24 16:19:08 -0600

    Update usuario.py
    update - Se agrega documentación y se añaden señaladores de abstracción y decoración.

--- unidad-3/usuario.py
+++ unidad-3/usuario.py
@@ -1,10 +1,27 @@
+#Se importan librerias de abstraccion
 from abc import ABC, abstractmethod
 
-class Usuario(ABC):
+#Se define la clase abstracta Usuario como subclase de ABC
+class Usuario(ABC): #ABSTRACCION
+    """
+    Representa un usuario en abstracto
+
+    Attributes:
+        nombre (String): El nombre del usuario.
+        correo (String): El correo electrónico del usuario.
+    
+    Methods:
+        mostrar_info(): Metodo abstracto de la clase, debe entregar un string.
+    """
+    #Se inicializa el constructor de la clase
     def __init__(self, nombre: str, correo: str):
+        #Se inicializa el nombre
         self.nombre = nombre
+        #Se inicializa el correo
         self.correo = correo
     
-    @abstractmethod
+    #Funcion abstracta para mostrar informacion decorada como metodo abstracto
+    @abstractmethod  #DECORADOR
     def mostrar_info(self) -> str:
+        #Se define de manera abstracta
         pass
