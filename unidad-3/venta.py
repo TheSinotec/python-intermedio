67ac1eae3ab3bcfbc2a7130b8feb3affa83a8bd5
Jonatan Salgado Razo <likelas@ciencias.unam.mx>
2025-11-25 11:49:44 -0600

    Update venta.py
    update - Se termina la documentacion

--- unidad-3/venta.py
+++ unidad-3/venta.py
@@ -18,17 +18,40 @@ class Venta:
     """
     #Se llama al constructor de la clase
     def __init__(self, cliente: Cliente): #AGREGACION
+        """
+        Parameters:
+            cliente (Cliente): El cliente como objeto Cliente
+            productos (list<Producto>): La lista de productos comprados como lista de objetos Producto.
+        """
         #Se inicializa el cliente
         self.cliente = cliente
         #Se inicializa una lista
         self.productos: list[Producto] = []
     
-    #Funcion para agregar un producto a la lista de productos
+    #Metodo para agregar un producto a la lista de productos
     def agregar_producto(self, producto: Producto) -> None:
+        """
+        Metodo que agrega un producto a la lista de productos
+        
+        Parameters:
+            producto (Producto): El producto que se agrega.
+        
+        Returns:
+            (None): No retorna nada.
+        """
         #Se agrega el producto a la lista
         self.productos.append(producto)
 
-    #Funcion para entregar el total de los precios de la lista de productos
+    #Metodo para entregar el total de los precios de la lista de productos
     def total(self) -> float:
+        """
+        Metodo que entrega el total de los precios de la lista de productos.
+        
+        Parameters:
+            (None): No recibe nada.
+        
+        Returns:
+            (Float): El resultado de la suma de los precios de la lista de productos.
+        """
         #Se regresa el total de la lista segun sus precios
         return sum(p.precio for p in self.productos)
